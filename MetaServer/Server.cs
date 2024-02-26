5100563228fbd589b1972b553c413e97e6457a22
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-02-26 20:14:48 +0300

    Update Server.cs

--- MetaServer/Server.cs
+++ MetaServer/Server.cs
@@ -2,28 +2,34 @@
 using System.Net.Sockets;
 using System.Text;
 
-public class Server(string ipAddress, int port)
+public class Server
 {
-    private readonly TcpListener _listener = new (IPAddress.Parse(ipAddress), port);
-    private readonly List<TcpClient> _clients = [];
+    private readonly TcpListener _listener;
+    private readonly List<TcpClient> _clients = new List<TcpClient>();
     private bool _isRunning = true;
+    private readonly object _lock = new object();
+
+    public Server(string ipAddress, int port)
+    {
+        _listener = new TcpListener(IPAddress.Parse(ipAddress), port);
+    }
 
     public void Start()
     {
         _listener.Start();
-
         Console.WriteLine("Сервер запущен. Ожидание подключений...");
 
         while (_isRunning)
         {
             TcpClient client = _listener.AcceptTcpClient();
-
-            _clients.Add(client);
+            lock (_lock)
+            {
+                _clients.Add(client);
+            }
 
             Console.WriteLine("Новый клиент подключен: " + client.Client.RemoteEndPoint);
 
-            Thread clientThread = new Thread(start: HandleClient);
-
+            Thread clientThread = new Thread(HandleClient);
             clientThread.Start(client);
         }
     }
@@ -33,39 +39,40 @@ public class Server(string ipAddress, int port)
         TcpClient client = (TcpClient)obj;
         NetworkStream stream = client.GetStream();
 
-        var buffer = new byte[1024];
+        byte[] buffer = new byte[1024];
         int bytesRead;
 
         try
         {
             while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
             {
-                var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                Console.WriteLine("Получено сообщение от клиента " +
-                    client.Client.RemoteEndPoint + ": " + message);
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Получено сообщение от клиента " + client.Client.RemoteEndPoint + ": " + message);
 
-                foreach (var c in _clients)
+                lock (_lock)
                 {
-                    if (c.Client.RemoteEndPoint == client.Client.RemoteEndPoint)
+                    foreach (TcpClient c in _clients)
                     {
-                        continue;
+                        if (c.Client.RemoteEndPoint != client.Client.RemoteEndPoint)
+                        {
+                            byte[] data = Encoding.UTF8.GetBytes(message);
+                            c.GetStream().Write(data, 0, data.Length);
+                        }
                     }
-
-                    var data = Encoding.UTF8.GetBytes(message);
-                    c.GetStream().Write(data, 0, data.Length);
                 }
             }
         }
-        catch (Exception exception)
+        catch (Exception e)
         {
-            Console.WriteLine("Ошибка чтения данных от клиента " + client.Client.RemoteEndPoint + ": " + exception.Message);
+            Console.WriteLine("Ошибка чтения данных от клиента " + client.Client.RemoteEndPoint + ": " + e.Message);
         }
         finally
         {
-            _clients.Remove(client);
-
+            lock (_lock)
+            {
+                _clients.Remove(client);
+            }
             Console.WriteLine("Клиент отключен: " + client.Client.RemoteEndPoint);
-
             client.Close();
         }
     }
@@ -73,12 +80,11 @@ public class Server(string ipAddress, int port)
     public void Stop()
     {
         _isRunning = false;
+        _listener.Stop();
 
         foreach (TcpClient client in _clients)
         {
             client.Close();
         }
-
-        _listener.Stop();
     }
 }
